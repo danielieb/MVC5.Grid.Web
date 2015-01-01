c8c46a968e48ff013a9954554bd782966bbbab13
Muchiachio <aldis.rameika@gmail.com>
2015-01-01 18:25:34 +0200

    Initial page styling
    - Added initial page styling.

--- Mvc.Grid.Web/Scripts/Shared/site.js
+++ Mvc.Grid.Web/Scripts/Shared/site.js
@@ -1,1 +1,12 @@
-﻿
+﻿$(document).ready(function () {
+    $('.main-menu').on('click', 'a', function (e) {
+        if ($(this).hasClass('dropdown-toggle') || $(this).closest('li').find('ul').length == 0) {
+            var current = $(this).next();
+            if (current.is(':visible')) {
+                $(this).closest('li.dropdown').find("ul.dropdown-menu").slideUp('fast');
+            } else {
+                current.slideDown('fast');
+            }
+        }
+    });
+});
