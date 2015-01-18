20bd780834d37a5314c67381da5da6a44d538a1b
Muchiachio <aldis.rameika@gmail.com>
2015-01-18 12:40:22 +0200

    Paging documentation
    - Added paging examples.

---
+++ Mvc.Grid.Web/Controllers/PagerController.cs
@@ -0,0 +1,27 @@
+﻿using NonFactors.Mvc.Grid.Web.Context;
+using System;
+using System.Web.Mvc;
+
+namespace Mvc.Grid.Web.Controllers
+{
+    public class PagerController : Controller
+    {
+        [HttpGet]
+        public ActionResult PagesToDisplay()
+        {
+            return View(PeopleRepository.GetPeople());
+        }
+
+        [HttpGet]
+        public ActionResult InitialPage()
+        {
+            return View(PeopleRepository.GetPeople());
+        }
+
+        [HttpGet]
+        public ActionResult RowsPerPage()
+        {
+            return View(PeopleRepository.GetPeople());
+        }
+    }
+}
