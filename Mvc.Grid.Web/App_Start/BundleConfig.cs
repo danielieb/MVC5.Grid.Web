b8e16bebac0565baa666ddbfaaf859e8be294879
Muchiachio <aldis.rameika@gmail.com>
2016-10-17 09:24:36 +0300

    Content bundling
    - Moved most of the content files to single file bundles, to decrease
    content requests on page load.

--- Mvc.Grid.Web/App_Start/BundleConfig.cs
+++ Mvc.Grid.Web/App_Start/BundleConfig.cs
@@ -11,19 +11,20 @@ namespace NonFactors.Mvc.Grid.Web
         }
         private static void RegisterScripts(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/Scripts/JQuery/Bundle").Include("~/Scripts/JQuery/*.js"));
-            bundles.Add(new ScriptBundle("~/Scripts/Bootstrap/Bundle").Include("~/Scripts/Bootstrap/*.js"));
-            bundles.Add(new ScriptBundle("~/Scripts/JQueryUI/Bundle").Include("~/Scripts/JQueryUI/*.js"));
-            bundles.Add(new ScriptBundle("~/Scripts/MvcGrid/Bundle").Include("~/Scripts/MvcGrid/*.js"));
-            bundles.Add(new ScriptBundle("~/Scripts/Shared/Bundle").Include("~/Scripts/Shared/*.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/Page/Bundle")
+                .Include("~/Scripts/JQuery/*.js")
+                .Include("~/Scripts/Bootstrap/*.js")
+                .Include("~/Scripts/MvcGrid/*.js")
+                .Include("~/Scripts/Shared/*.js"));
         }
         private static void RegisterStyles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/JQueryUI/Bundle").Include("~/Content/JQueryUI/*.css"));
-            bundles.Add(new StyleBundle("~/Content/Bootstrap/Bundle").Include("~/Content/Bootstrap/*.css"));
-            bundles.Add(new StyleBundle("~/Content/FontAwesome/Bundle").Include("~/Content/FontAwesome/*.css"));
-            bundles.Add(new StyleBundle("~/Content/MvcGrid/Bundle").Include("~/Content/MvcGrid/*.css"));
-            bundles.Add(new StyleBundle("~/Content/Shared/Bundle").Include("~/Content/Shared/*.css"));
+            bundles.Add(new StyleBundle("~/Content/Page/Bundle")
+                .Include("~/Content/JQueryUI/*.css")
+                .Include("~/Content/Bootstrap/*.css")
+                .Include("~/Content/FontAwesome/*.css")
+                .Include("~/Content/MvcGrid/*.css")
+                .Include("~/Content/Shared/*.css"));
         }
     }
 }
