2fb7eb4f050d126e6131e181ae931c8b4fba3e6e
Muchiachio <aldis.rameika@gmail.com>
2017-01-09 14:52:09 +0200

    Proper localization samples
    - Fixed, localization sample not using newly added "not equals filter".

--- Mvc.Grid.Web/Scripts/MvcGrid/Cultures/mvc-grid.culture.fr.js
+++ Mvc.Grid.Web/Scripts/MvcGrid/Cultures/mvc-grid.culture.fr.js
@@ -2,11 +2,13 @@
     Text: {
         Contains: 'Contient',
         Equals: 'Egale',
+        NotEquals: 'Pas égal',
         StartsWith: 'Commence par',
         EndsWith: 'Fini par'
     },
     Number: {
         Equals: 'Egale',
+        NotEquals: 'Pas égal',
         LessThan: 'Inférieur à',
         GreaterThan: 'Supérieur à',
         LessThanOrEqual: 'Inférieur ou égale à',
@@ -14,6 +16,7 @@
     },
     Date: {
         Equals: 'Egale',
+        NotEquals: 'Pas égal',
         LessThan: 'Inférieur à',
         GreaterThan: 'Supérieur à',
         LessThanOrEqual: 'Inférieur ou égale à',
